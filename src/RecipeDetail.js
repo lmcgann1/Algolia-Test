b0592d5d6d9fcae66b62653ebf4a98f7b20f279f
Lorcan McGann <mcgann.lorgan@gmail.com>
2023-12-18 17:31:42 -0500

    Configure React app for deployment to GitHub Pages

---
+++ src/RecipeDetail.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RecipeDetail = ({ hit }) => {
+  return (
+    <div>
+      <h1>{hit.recipe_name} Details</h1>
+    </div>
+  );
+};
+
+export default RecipeDetail;
